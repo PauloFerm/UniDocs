9bef5ac71e71dfa96f1cb2e560d369603dfd0ccc
Paulo <paulo.fzmz@proton.me>
2023-06-16 12:16:51 -0400

    Enumeration ask if ingore #REF items

--- src/1-Macros/verification.ts
+++ src/1-Macros/verification.ts
@@ -1,5 +1,6 @@
 namespace Validation {
   export function enumeration() {
+    let ui = SpreadsheetApp.getUi();
     let thisSpreadsheet = SpreadsheetApp.getActive();
     let costSheet = thisSpreadsheet.getSheetByName("Presupuesto");
     let eettSheet = thisSpreadsheet.getSheetByName("EETT");
@@ -18,7 +19,7 @@ namespace Validation {
       throw "Problem with Costs or EETT Sheet size";
     }
 
-    function clean1DArray(array: any[][]) {
+    function clean1DArray(array: any[][], ignoreREF: boolean) {
       let array1D = array.map(line => line[0]);
       let clean = array1D.filter(item => item != "")
                         .filter(item => item.indexOf("SUBTOTAL") == -1)
@@ -26,18 +27,24 @@ namespace Validation {
                         .filter(item => item.indexOf("TOTAL EQUIPOS") == -1)
                         .filter(item => item.indexOf("TOTAL EQUIPAMIENTO") == -1);
 
+      if (ignoreREF) {
+        clean = clean.filter(item => item.indexOf("#REF!") == -1);
+      }
       return clean
     }
 
-    costItems = clean1DArray(costItems);
-    eettItems = clean1DArray(eettItems);
+    let alert = ui.alert("Ignorar Vínculos Rotos (#REF!)", ui.ButtonSet.YES_NO);
+    let response = (alert == ui.Button.YES);
+
+    costItems = clean1DArray(costItems, response);
+    eettItems = clean1DArray(eettItems, response);
 
     // Logger.log(costItems);
     // Logger.log(eettItems);
 
     for (let item in costItems) {
       if (costItems[item] != eettItems[item]) {
-        throw `Non correlative items at ${costItems[item]} - ${eettItems[item]}`;
+        throw `Items no correlativos ${costItems[item]} - ${eettItems[item]}`;
       }
     }
 
