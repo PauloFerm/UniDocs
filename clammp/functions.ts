6006b935d9aa52956ff54771c465563d4c6da107
Paulo <paulo.fzmz@proton.me>
2023-08-14 18:03:12 -0400

    2 space tabs

--- clammp/functions.ts
+++ clammp/functions.ts
@@ -5,52 +5,52 @@ export interface Project { name: string, id: string };
 export interface ClaspFile { scriptId: string, rootDir: string };
 
 export const currentProject = () => {
-    const projects: Project[] = require('./projectsIds.json');
-    const clasp: ClaspFile = require('../.clasp.json');
+  const projects: Project[] = require('./projectsIds.json');
+  const clasp: ClaspFile = require('../.clasp.json');
 
-    let current = projects.find(project => project.id == clasp.scriptId);
+  let current = projects.find(project => project.id == clasp.scriptId);
 
-    if (current == undefined) { throw "Unregistered Project" }
-    
-    return current
+  if (current == undefined) { throw "Unregistered Project" }
+  
+  return current
 }
 
 const overwriteId = (path: string, id: string) => {
-    let data = readFileSync(path, { encoding: 'utf-8' }); 
-    let idRegex = /"scriptId":"[a-zA-Z0-9_-]*"/;
-    let newData = data.replace(idRegex, `"scriptId":"${id}"`);
-    
-    writeFileSync(path, newData, 'utf-8');
+  let data = readFileSync(path, { encoding: 'utf-8' }); 
+  let idRegex = /"scriptId":"[a-zA-Z0-9_-]*"/;
+  let newData = data.replace(idRegex, `"scriptId":"${id}"`);
+  
+  writeFileSync(path, newData, 'utf-8');
 }
 
 export const changeClaspProject = (id: string) => {
-    // Executed in root directory
-    overwriteId('./.clasp.json', id); 
-    overwriteId('./gs/.clasp.json', id);
+  // Executed in root directory
+  overwriteId('./.clasp.json', id); 
+  overwriteId('./gs/.clasp.json', id);
 }
 
 const runCommand = async (cmd: string) => {
-    exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error.message}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-    });
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
 }
 
 export const updateCurrentProject = () => {
-    let command = "clasp push && cd gs && clasp pull && cd ..";
-    runCommand(command);
+  let command = "clasp push && cd gs && clasp pull && cd ..";
+  runCommand(command);
 }
 
 export const deployToAllProjects = async () => {
-    const projects: Project[] = require('./projectsIds.json');
+  const projects: Project[] = require('./projectsIds.json');
 
-    console.log("Proyectos:");
-    for (let project of projects) {
-        await changeClaspProject(project.id);
-        runCommand("clasp push");
-    }
+  console.log("Proyectos:");
+  for (let project of projects) {
+    await changeClaspProject(project.id);
+    runCommand("clasp push");
+  }
 }
--- clammp/index.ts
+++ clammp/index.ts
@@ -1,65 +1,62 @@
 // const inquirer = require('@inquirer/prompts');
 import { select } from "@inquirer/prompts";
-import * as deploy from "./deploy";
+import * as functions from "./functions";
 
-import { Project } from "./deploy";
+import { Project } from "./functions";
 
 const selectProjectMenu = async () => {
-    let projects: Project[] = require('./projectsIds.json'); 
-        //.map(project => project.name);
+  let projects: Project[] = require('./projectsIds.json'); 
+  //.map(project => project.name);
 
-    const selectedProject = await select({
-        message: "Select a Project to deploy:",
-        choices: projects.map(project => {
-            return {
-                name: project.name,
-                value: project
-            }
-        })
-    });
+  const selectedProject = await select({
+    message: "Select a Project to deploy:",
+    choices: projects.map(project => {
+      return {
+        name: project.name,
+        value: project
+      }
+    })
+  });
 
-    return selectedProject
+  return selectedProject
 }
 
 const mainMenu = async () => {
-    let currentProject = deploy.currentProject();
+  let currentProject = functions.currentProject();
 
-    const option = await select({
-        message: `Current project: ${currentProject.name}`,
-        choices: [
-            {
-                name: "Update current project",
-                value: "update"
-            },
-            {
-                name: "Deploy to all projects",
-                value: "deploy"
-            },
-            {
-                name: "Change current project",
-                value: "change"
-            }
-        ]
-    });
+  const option = await select({
+    message: `Current project: ${currentProject.name}`,
+    choices: [
+      {
+        name: "Update current project",
+        value: "update"
+      },
+      {
+        name: "Deploy to all projects",
+        value: "deploy"
+      },
+      {
+        name: "Change current project",
+        value: "change"
+      }
+    ]
+  });
 
-    switch (option) {
-        case "update":
-            deploy.updateCurrentProject();
-            break;
-        case "deploy":
-            deploy.deployToAllProjects();
-            break;
-        case "change":
-            let selectedProject = await selectProjectMenu();
-            deploy.changeClaspProject(selectedProject.id);
-            // Continue loging current project
-        // case "current":
-        //     let currentProject = deploy.currentProject();
-        //     console.log("Current project:", currentProject.name)
-        //     break;
-        default:
-            console.log("No action for", option);
-    }
+  switch (option) {
+    case "update":
+      functions.updateCurrentProject();
+      break;
+    case "deploy":
+      functions.deployToAllProjects();
+      break;
+    case "change":
+      let selectedProject = await selectProjectMenu();
+      functions.changeClaspProject(selectedProject.id);
+      console.log(`Current project: ${selectedProject.name}`);
+      break;
+    default:
+      console.log("No action for", option);
+  }
 }
 
 export const launchMenu = mainMenu();
